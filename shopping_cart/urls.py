dd2fe2f803ddb19a28f56b786ab9ed8a36dc99ab
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 10:27:42 +0100

    created the shopping cart app to handle the shopping cart

---
+++ shopping_cart/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import add_to_cart, remove_from_cart, order_view, checkout
+
+app_name = "cart"
+
+urlpatterns = [
+    path("add-to-cart/<book_slug>", add_to_cart, name="add-to-cart"),
+    path("remove-from-cart/<book_slug>",
+         remove_from_cart, name="remove-from-cart"),
+    path("order_summary/", order_view, name="order-summary"),
+    path("checkout/", checkout, name="checkout")
+
+]
